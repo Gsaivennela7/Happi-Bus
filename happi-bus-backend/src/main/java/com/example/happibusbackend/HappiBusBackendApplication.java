24188adcf78caa7048ff41c19c407f2bf4d009de
Gsaivennela7 <saivennela.garikapati@sjsu.edu>
2023-02-26 18:26:43 -0800

    Initial project commit

---
+++ happi-bus-backend/src/main/java/com/example/happibusbackend/HappiBusBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.happibusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappiBusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HappiBusBackendApplication.class, args);
+	}
+
+}
