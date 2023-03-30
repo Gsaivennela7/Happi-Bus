fff396a47a3f7713cf30525c47edbb1b21f27401
Gsaivennela7 <saivennela.garikapati@sjsu.edu>
2023-03-29 21:10:29 -0700

    database

---
+++ happi-bus-backend/src/main/java/com/example/controller/PassengerController.java
@@ -0,0 +1,20 @@
+import java.com.example.model.*;
+
+@RestController
+public class PassengerController{
+
+
+@Autowired
+Account account;
+
+
+
+@Autowired
+Ticket ticket;
+
+
+
+                    
+
+
+}
